8b4537f525c445ae88f99216e071f7937d45310a
YouJedong <mraiou1@naver.com>
2025-01-30 15:09:00 +0900

    update: 강의8회차 - 스프링 데이터 JPA 리포지토리로 변경 - 사용자 정의 리포지토리 - 스프링 데이터 페이징 활용1 - Querydsl 페이징 연동 - 스프링 데이터 페이징 활용2 - CountQuery 최적화 - 스프링 데이터 페이징 활용3 - 컨트롤러 개발

---
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -0,0 +1,138 @@
+package study.querydsl.repository;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
+
+import java.util.List;
+
+import static org.springframework.util.StringUtils.hasText;
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
+
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    public MemberRepositoryImpl(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+
+    @Override
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoc(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> results = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoc(condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        List<MemberTeamDto> contents = results.getResults();
+        long total = results.getTotal();
+        return new PageImpl<>(contents, pageable, total);
+
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> contents = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoc(condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        JPAQuery<Member> countQuery = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoc(condition.getAgeLoe())
+                );
+
+//        return new PageImpl<>(contents, pageable, total);
+        return PageableExecutionUtils.getPage(contents, pageable, () -> countQuery.fetchCount());
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        if (ageGoe == null) return null;
+        return member.age.goe(ageGoe);
+    }
+
+    private BooleanExpression ageLoc(Integer ageLoe) {
+        if (ageLoe == null) return null;
+        return member.age.loe(ageLoe);
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        if (!hasText(teamName)) return null;
+        return member.team.name.eq(teamName);
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        if (!hasText(username)) return null;
+        return member.username.eq(username);
+    }
+
+
+
+
+}
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,10 +1,12 @@
 package study.querydsl.repository;
 
 import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -13,79 +15,35 @@ import study.querydsl.entity.Team;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-class RepositoryTest {
+class MemberRepositoryTest {
+
 
     @Autowired
     EntityManager em;
 
     @Autowired
-    MemberJpaRepository memberJpaRepository;
+    MemberRepository memberRepository;
 
     @Test
     public void basicTest() {
         Member member = new Member("member1", 10);
-        memberJpaRepository.save(member);
+        memberRepository.save(member);
 
-        Member member1 = memberJpaRepository.findById(member.getId()).get();
+        Member member1 = memberRepository.findById(member.getId()).get();
         System.out.println("member1 = " + member1);
 
-        List<Member> all = memberJpaRepository.findAll();
+        List<Member> all = memberRepository.findAll();
         assertThat(all).containsExactly(member);
 
-        List<Member> findMember1 = memberJpaRepository.findUsername(member.getUsername());
+        List<Member> findMember1 = memberRepository.findByUsername(member.getUsername());
         assertThat(findMember1).containsExactly(member);
 
     }
-
-    @Test
-    public void basicQuerydslTest() {
-        Member member = new Member("member1", 10);
-        memberJpaRepository.save(member);
-
-        List<Member> result1 = memberJpaRepository.findAll_querydsl();
-        assertThat(result1).containsExactly(member);
-
-        List<Member> result2 = memberJpaRepository.findUsername_querydsl(member.getUsername());
-        assertThat(result2).containsExactly(member);
-
-    }
-
-    @Test
-    public void searchByBuilder() {
-
-        Team teamA = new Team("teamA");
-        Team teamB = new Team("teamB");
-        em.persist(teamA);
-        em.persist(teamB);
-
-        Member member1 = new Member("member1", 10, teamA);
-        Member member2 = new Member("member2", 20, teamA);
-
-        Member member3 = new Member("member3", 30, teamB);
-        Member member4 = new Member("member4", 40, teamB);
-        em.persist(member1);
-        em.persist(member2);
-        em.persist(member3);
-        em.persist(member4);
-
-        MemberSearchCondition searchCond = new MemberSearchCondition();
-//        searchCond.setUsername("member4");
-//        searchCond.setTeamName("teamB");
-//        searchCond.setAgeGoe(35);
-//        searchCond.setAgeLoe(40);
-
-        List<MemberTeamDto> members = memberJpaRepository.searchByBuilder(searchCond);
-        for (MemberTeamDto member : members) {
-            System.out.println("member = " + member);
-        }
-        assertThat(members).extracting("username").containsExactly("member4");
-    }
-
 
     @Test
     public void searchByParameter() {
@@ -111,11 +69,43 @@ class RepositoryTest {
         searchCond.setAgeGoe(35);
 //        searchCond.setAgeLoe(40);
 
-        List<MemberTeamDto> members = memberJpaRepository.findBySearch(searchCond);
+        List<MemberTeamDto> members = memberRepository.search(searchCond);
         for (MemberTeamDto member : members) {
             System.out.println("member = " + member);
         }
         assertThat(members).extracting("username").containsExactly("member4");
     }
 
+    @Test
+    public void search() {
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition searchCond = new MemberSearchCondition();
+//        searchCond.setUsername("member4");
+//        searchCond.setTeamName("teamB");
+//        searchCond.setAgeGoe(35);
+//        searchCond.setAgeLoe(40);
+
+        PageRequest page = PageRequest.of(0, 3);
+
+        Page<MemberTeamDto> result = memberRepository.searchPageSimple(searchCond , page);
+
+        assertThat(result.getSize()).isEqualTo(3);
+        assertThat(result.getContent()).extracting("username").containsExactly("member1","member2","member3");
+
+    }
 }
